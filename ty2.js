453a3459dc1eb1bb37d7685bb4025a5dace69877
PhuongPham <phuongpham20012001@gmail.com>
2020-09-17 12:24:12 +0700

    ty ty

---
+++ ty2.js
@@ -0,0 +1,11 @@
+var  number_to_print  =  0 ;
+
+process.stdout.write( "\n Numbers from 0 to 20 are the following:\n\n ") ;
+
+while ( number_to_print  <= 20 )
+{
+   process.stdout.write( " "  +  number_to_print ) ;
+   number_to_print  ++   ;
+}
+
+process.stdout.write( "\n\n End of program.\n\n ") ;
